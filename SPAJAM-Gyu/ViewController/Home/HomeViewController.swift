c0aea64be393717f667f8b6cce7dd26ac1ed9a32
Yuki Omura <moti0068@gmail.com>
2019-05-12 02:22:19 +0900

    wip

---
+++ SPAJAM-Gyu/ViewController/Home/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  SPAJAM-Gyu
+//
+//  Created by 大村優輝 on 2019/05/12.
+//  Copyright © 2019 KazumaOhashi. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
