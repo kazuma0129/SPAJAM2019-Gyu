8c3465eabc761eef69b9efcc8d8f1a913ef74f77
KazumaOhashi <b1016095@fun.ac.jp>
2019-05-12 04:28:40 +0900

    [feat]CloudStrageに写真が投稿できる

--- SPAJAM-Gyu/ViewController/Camera/CameraViewController.swift
+++ SPAJAM-Gyu/ViewController/Camera/CameraViewController.swift
@@ -7,44 +7,95 @@
 //
 
 import UIKit
+import Photos
 import Firebase
 import FirebaseStorage
 
-class CameraViewController: UIViewController{
+//@objc(ViewController)
+class CameraViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
         let c = UIImagePickerController()
+        c.delegate = self
 //        c.mediaTypes = ["public.movie"]
-        present(c, animated: true)
-        
-    }
-
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // [1]ストレージ サービスへの参照を取得
-        let storage = Storage.storage()
-        
-        // [2]ストレージへの参照を取得
-        let storageRef = storage.reference(forURL: "gs://<your-firebase-storage-bucket>")
-        
-        // [3]ツリーの下位への参照を作成
-        let imageRef = storageRef.child("image.jpg")
-        
-        // [4]Dataを作成
-        let imageData = UIImageJPEGRepresentation(UIImage(named: "image")!, 1.0)!
-        
-        // [5]アップロードを実行
-        imageRef.put(imageData, metadata: nil) { metadata, error in
-            if (error != nil) {
-                print("Uh-oh, an error occurred!")
-            } else {
-                let downloadURL = metadata!.downloadURL()!
-                print("downloadURL:", downloadURL)
-            }
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            c.sourceType = .camera
+        } else {
+            c.sourceType = .photoLibrary
         }
         
+        present(c, animated: true, completion:nil)
         
     }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        let info = convertFromUIImagePickerControllerInfoKeyDictionary(info)
+        
+        if #available(iOS 8.0, *), let referenceUrl = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.referenceURL)] as? URL {
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceUrl], options: nil)
+            let asset = assets.firstObject
+            asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
+                let imageFile = contentEditingInput?.fullSizeImageURL
+                let filePath = "test_user" + "/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\(imageFile!.lastPathComponent)"
+                let storage = Storage.storage()
+                // [START uploadimage]
+                let storageRef = storage.reference(withPath: filePath)
+                storageRef.putFile(from: imageFile!, metadata: nil) { (metadata, error) in
+                    if let error = error {
+                        print("Error uploading: \(error)")
+                        return
+                    }
+                    self.uploadSuccess(storageRef, storagePath: filePath)
+                }
+                // [END uploadimage]
+            })
+        } else {
+            guard let image = info[convertFromUIImagePickerControllerInfoKey(UIImagePickerController.InfoKey.originalImage)] as? UIImage else { return }
+            guard let imageData = image.jpegData(compressionQuality: 0.8) else { return }
+            
+            let imagePath = "image/samp.jpg"
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
+            let storage = Storage.storage()
+            let storageRef = storage.reference(withPath: imagePath)
+            storageRef.putData(imageData, metadata: metadata) { (metadata, error) in
+                if let error = error {
+                    print("Error uploading: \(error)")
+                    return
+                }
+                self.uploadSuccess(storageRef, storagePath: imagePath)
+            }
+        }
+        picker.dismiss(animated: true , completion: nil)
+
+        
+    }
+    func uploadSuccess(_ storageRef: StorageReference, storagePath: String) {
+        print("Upload Succeeded!")
+        storageRef.downloadURL { (url, error) in
+            if let error = error {
+                print("Error getting download URL: \(error)")
+                return
+            }
+            UserDefaults.standard.set(storagePath, forKey: "storagePath")
+            UserDefaults.standard.synchronize()
+        }
+        self.dismiss(animated: true , completion: nil)
+    }
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        picker.dismiss(animated: true, completion:nil)
+    }
+}
+
+fileprivate func convertFromUIImagePickerControllerInfoKeyDictionary(_ input: [UIImagePickerController.InfoKey: Any]) -> [String: Any] {
+    return Dictionary(uniqueKeysWithValues: input.map {key, value in (key.rawValue, value)})
+}
+
+// Helper function inserted by Swift 4.2 migrator.
+fileprivate func convertFromUIImagePickerControllerInfoKey(_ input: UIImagePickerController.InfoKey) -> String {
+    return input.rawValue
 }
--- SPAJAM-Gyu/ViewController/Camera/cameraExtention.swift
+++ SPAJAM-Gyu/ViewController/Camera/cameraExtention.swift
@@ -1,19 +1,49 @@
+////
+////  cameraExtention.swift
+////  SPAJAM-Gyu
+////
+////  Created by 大村優輝 on 2019/05/11.
+////  Copyright © 2019 KazumaOhashi. All rights reserved.
+////
 //
-//  cameraExtention.swift
-//  SPAJAM-Gyu
+//import UIKit
+//import Firebase
+//import FirebaseStorage
 //
-//  Created by 大村優輝 on 2019/05/11.
-//  Copyright © 2019 KazumaOhashi. All rights reserved.
+//extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+//        let image = info[.originalImage] as? UIImage
+//        
+//        let mediaType = info[.originalImage] as? String // → public.image
+//        guard let imageUrl = info[.originalImage] as? URL else { return } // → assets-library://asset/asset.JPG?id=XXXX&ext=JPG
+//        
+//        // [1]ストレージ サービスへの参照を取得
+//        let storage = Storage.storage()
+//        
+//        // [2]ストレージへの参照を取得
+//        let storageRef = storage.reference(forURL: "gs://spajam-gyu.appspot.com")
+//        // File located on disk
+////        let localFile =
+//        
+//        // Create a reference to the file you want to upload
+//        let riversRef = storageRef.child("images/rivers.jpg")
+//        
+//        // Upload the file to the path "images/rivers.jpg"
+//        let uploadTask = riversRef.putFile(from: imageUrl, metadata: nil) { metadata, error in
+//            guard let metadata = metadata else {
+//                // Uh-oh, an error occurred!
+//                return
+//            }
+//            // Metadata contains file metadata such as size, content-type.
+//            let size = metadata.size
+//            // You can also access to download URL after upload.
+//            storageRef.downloadURL { (url, error) in
+//                guard let downloadURL = url else {
+//                    // Uh-oh, an error occurred!
+//                    return
+//                }
+//                print(downloadURL)
+//            }}
 //
-
-import UIKit
-
-extension ViewController: UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-        let image = info[.originalImage] as? UIImage
-        
-        let mediaType = info[.originalImage] as? String // → public.image
-        let imageUrl = info[.originalImage] as? URL // → assets-library://asset/asset.JPG?id=XXXX&ext=JPG
-        print(mediaType)
-    }
-}
+//    }
+//}
