0436a9fb42aec8fbcb19bee9546e503ad888573b
Yuki Omura <moti0068@gmail.com>
2019-05-11 22:05:38 +0900

    カメラロール表示

--- SPAJAM-Gyu/ViewController/Camera/CameraViewController.swift
+++ SPAJAM-Gyu/ViewController/Camera/CameraViewController.swift
@@ -8,23 +8,19 @@
 
 import UIKit
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController{
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let c = UIImagePickerController()
+        c.mediaTypes = ["public.image", "public.movie"]
+        present(c, animated: true)
+        
+    }
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
